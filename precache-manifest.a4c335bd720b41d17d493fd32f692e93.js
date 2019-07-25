8d1492ea691fc1c17fa8ef387c67b6d806d60916
kaio <khaledrandji@gmail.com>
2019-07-25 12:48:08 +0200

    Updates

---
+++ precache-manifest.a4c335bd720b41d17d493fd32f692e93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ca9a80e9a3d5f1e3500bf9df964cc7",
+    "url": "/monsters-roldex/index.html"
+  },
+  {
+    "revision": "a4326a9a53ab2e49ea88",
+    "url": "/monsters-roldex/static/css/main.cd7fb4d2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-roldex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a4326a9a53ab2e49ea88",
+    "url": "/monsters-roldex/static/js/main.00888646.chunk.js"
+  },
+  {
+    "revision": "c416963589db26118f22",
+    "url": "/monsters-roldex/static/js/runtime~main.6cd28cb1.js"
+  }
+]);
